e753a898e6f8f4b6f3a6f39487947e9b17941a70
Miquel Ferriol <mik_ferri@hotmail.com>
2022-02-10 21:39:20 +0100

    Changed scalability test results

--- Scalability/link_division_main.py
+++ Scalability/link_division_main.py
@@ -1,5 +1,5 @@
 import sys
-
+import re
 sys.path.insert(1, "./code")
 from link_division_model import LinkDivModel
 import configparser
@@ -41,6 +41,7 @@ ds_train = input_fn('../data/scalability/train/', min_scale=min_scale, max_scale
 ds_train = ds_train.map(lambda x, y: transformation(x, y))
 ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
 ds_train = ds_train.repeat()
+ds_train = ds_train.shuffle(1000)
 
 ds_test = input_fn('../data/scalability/test/300', min_scale=10, max_scale=11, shuffle=False)
 ds_test = ds_test.map(lambda x, y: transformation(x, y))
@@ -70,8 +71,19 @@ ckpt_dir = 'ckpt_dir'
 latest = tf.train.latest_checkpoint(ckpt_dir)
 
 if latest is not None:
-    print("Found a pretrained model, restoring...")
-    model.load_weights(latest)
+    print("Found a pretrained model, restoring...")best = None
+    best_mre = float('inf')
+    for f in os.listdir('./ckpt_dir'):
+        if os.path.isfile(os.path.join('./ckpt_dir', f)):
+            reg = re.findall("\d+\.\d+", f)
+            if len(reg) > 0:
+                mre = float(reg[0])
+                if mre <= best_mre:
+                    best = f.replace('.index', '')
+                    best_mre = mre
+
+    print("BEST CHECKOINT FOUND: {}".format(best))
+    model.load_weights('./ckpt_dir/{}'.format(best))
 else:
     print("Starting training from scratch...")
 
