73696cfae3c15e948261d59f648017371f2b96f5
Miquel Ferriol-Galmés <mferriol@ac.upc.edu>
2022-02-14 18:44:25 +0100

    Dataset

--- check_error.py
+++ check_error.py
@@ -1,15 +1,18 @@
 import re
 import os
+
+TOPOLOGIES = [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 220, 240,
+              260, 280, 300]
 errors = []
 for line in open('./Scalability/check_error.out'):
     match = re.search('MAPE: (\d+\.\d+)', line)
     if match:
         errors.append(float(match.group(1)))
-
+errors = list(dict.fromkeys(errors))
+print(len(errors))
+print(len(TOPOLOGIES))
 errors = [errors[i:i + 20] for i in range(0, len(errors), 20)]
-for i, t in enumerate(
-        [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 220, 240, 260,
-         280, 300]):
+for i, t in enumerate(TOPOLOGIES):
     print("TOPO SIZE: {}".format(t))
     count = 0
     for j, error in enumerate(errors[i]):
